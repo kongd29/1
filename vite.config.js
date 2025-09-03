56587be5c97e6738268a7411721a510533c914c4
kongd29 <qaz8991@naver.com>
2025-09-03 10:20:24 +0900

    fix: module scripts & multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        versionA: resolve(__dirname, 'version-a.html'),
+        versionB: resolve(__dirname, 'version-b.html'),
+      },
+    },
+  },
 });
