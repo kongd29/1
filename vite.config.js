081013cee844a4f562632b0123f541727a9c7362
kongd29 <qaz8991@naver.com>
2025-09-03 09:52:17 +0900

    vite build: ensure outDir and netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-export default {
-  root: "public",
-  server: {
-    port: 5173,
-    open: "/index.html",
-    proxy: {
-      "/track": "http://127.0.0.1:8000",
-      "/metrics": "http://127.0.0.1:8000"
-    }
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
-}
+});
